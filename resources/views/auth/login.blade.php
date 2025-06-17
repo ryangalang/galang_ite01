79eb7c5c8eba551e2e2eacafcda7ff9b9594d3fc
John Reynan <John@test>
2025-06-17 16:27:01 +0800

    Add bagong feature/update sa Laravel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -77,7 +77,7 @@
                         <!--end::Row-->
                     </form>
                     <!-- /.social-auth-links -->
-                    <p class="mb-1"><a href="forgot-password.html">I forgot my password</a></p>
+                    <p class="mb-1"><a href="{{ route('password.request') }}">I forgot my password</a></p>
                     <p class="mb-0">
                         <a href="{{ url('register') }}" class="text-center"> Register a new membership </a>
                     </p>
