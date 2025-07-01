1bc979c09e6cc04e36605e9c31bf17a7633fa549
John Reynan <John@test>
2025-07-01 16:41:28 +0800

    hi

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if(Auth::attempt($credentials)){
+            $user = Auth::user();
+            $token = $user->createToken('GalangITE01')->plainTextToken;
+
+            return response()->json(['token' => $token], 200);
+
+        }
+
+        return response()->json(['error' => 'Unauthorize'], 401);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
